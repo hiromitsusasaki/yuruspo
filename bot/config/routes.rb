ebab40f7af9a4f2686216352053631f80b1eb441
nbeat <dev.nbeat@gmail.com>
2018-10-09 08:07:32 +0900

    ジョブキューシステム用にRedisとsidekiqのコンテナを追加

--- bot/config/routes.rb
+++ bot/config/routes.rb
@@ -1,5 +1,6 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   post '/line/callback', to: 'line_bot_webhook#callback'
-
+  mount Sidekiq::Web, at: '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
