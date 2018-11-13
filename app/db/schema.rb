e382d2289f70dddb089a11922cffc784d24ec5d0
nbeat <dev.nbeat@gmail.com>
2018-11-13 15:35:44 +0900

    Userモデルにdeviseで自動生成されたもの以外のカラムを追加

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113062553) do
+ActiveRecord::Schema.define(version: 20181113062957) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "circle_id"
@@ -168,6 +168,12 @@ ActiveRecord::Schema.define(version: 20181113062553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "display_name"
+    t.text "status_message"
+    t.string "picture_url"
+    t.boolean "is_circle_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
