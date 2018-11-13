bc5019990bbef83611a86d1fc70a884138a9aa6f
nbeat <dev.nbeat@gmail.com>
2018-11-13 23:06:15 +0900

    deviseをアンインストール

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113062957) do
+ActiveRecord::Schema.define(version: 20181113135120) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "circle_id"
@@ -161,11 +161,6 @@ ActiveRecord::Schema.define(version: 20181113062957) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
@@ -174,8 +169,6 @@ ActiveRecord::Schema.define(version: 20181113062957) do
     t.text "status_message"
     t.string "picture_url"
     t.boolean "is_circle_admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "activities", "circles"
