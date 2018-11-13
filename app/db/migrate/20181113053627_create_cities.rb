906121539a376725ea4cda4c7c05625e2044146a
nbeat <dev.nbeat@gmail.com>
2018-11-13 14:53:04 +0900

    Cityモデルを追加

---
+++ app/db/migrate/20181113053627_create_cities.rb
@@ -0,0 +1,10 @@
+class CreateCities < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cities do |t|
+      t.string :name
+      t.belongs_to :prefecture, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
