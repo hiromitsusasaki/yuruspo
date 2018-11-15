88b70bfc53527b5b483eb91fa3dc26dbd9fba975
nbeat <dev.nbeat@gmail.com>
2018-11-15 12:33:34 +0900

    表示テスト用ページを追加

---
+++ app/app/controllers/webapp/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class Webapp::StaticPagesController < ApplicationController
+  def test
+    
+  end
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   post '/line/callback', to: 'bot/line_webhook#callback'
   mount Sidekiq::Web, at: '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'static_pages/test', to: 'webapp/static_pages#test'
 end
