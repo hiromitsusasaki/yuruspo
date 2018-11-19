bdd84fe18c107740cf58910584ed9568916a649a
nbeat <dev.nbeat@gmail.com>
2018-11-19 23:53:14 +0900

    参加者ユーザーでログインした場合とサークルユーザーでログインした場合の処理の切り分けを追加

--- app/app/controllers/webapp/sessions_controller.rb
+++ app/app/controllers/webapp/sessions_controller.rb
@@ -2,7 +2,17 @@ class Webapp::SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    # TODO: login元で遷移先を分ける（UserとCIrcle）
+    login_as = session[:login_as] 
+    session.delete(:login_from)
+    case login_as 
+    when 'user' then
+      # userでログインした場合の処理
+      p 'login as user.'
+    when 'circle' then
+      # circleでログインした場合の処理
+      p 'login as circle.'
+    end
+    
     redirect_to root_path
   end
 
