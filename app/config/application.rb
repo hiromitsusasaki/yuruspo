e97340bd87f271fa37599be90594ee549d5e8de2
nbeat <dev.nbeat@gmail.com>
2018-11-21 23:06:38 +0900

    通常のモデル生成時はActiveRecordをORMとして使うように設定

--- app/config/application.rb
+++ app/config/application.rb
@@ -14,5 +14,9 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.gererators do |g|
+      g.orm :active_record
+    end
   end
 end
