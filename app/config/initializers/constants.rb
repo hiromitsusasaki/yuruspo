02c8c7896e9a135343ee35a6a678ca56c72f5a35
nakata yuki <nasutaro211@gmail.com>
2018-11-23 09:33:45 +0900

    make constants.rb and adapt it to help menu

---
+++ app/config/initializers/constants.rb
@@ -0,0 +1,7 @@
+module Constants
+  module S3
+    module ForUser
+      HELP_MENUS_URL = "https://s3-ap-northeast-1.amazonaws.com/yurusupo-bot-public-images/for_user/help_menus"
+    end
+  end
+end
