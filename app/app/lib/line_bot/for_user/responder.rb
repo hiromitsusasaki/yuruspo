db2e6d2017ef59a986dc3d6d2ffb85dc322e20a4
nakata yuki <nasutaro211@gmail.com>
2018-11-24 20:47:22 +0900

    add unfollow response

--- app/app/lib/line_bot/for_user/responder.rb
+++ app/app/lib/line_bot/for_user/responder.rb
@@ -11,6 +11,7 @@ class LineBot::ForUser::Responder
     responses.push(LineBot::ForUser::Response::ImageResponse.new)
     responses.push(LineBot::ForUser::Response::TextResponse::HelpResponse.new)
     responses.push(LineBot::ForUser::Response::FollowResponse.new)
+    responses.push(LineBot::ForUser::Response::UnfollowResponse.new)
     responses.push(LineBot::ForUser::Response::ErrorResponse.new)
     set_chain_of responses
     responses[0].send(@line_bot_event)
