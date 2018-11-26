012b524ccde50615695275cf11a81edcd6d5f650
nakata yuki <nasutaro211@gmail.com>
2018-11-26 23:23:13 +0900

    make seed file that make records: one circle and two activities. #90

---
+++ app/db/fixtures/sample_data/add_sample_data.rb
@@ -0,0 +1,32 @@
+# 最初に自分のユーザーレコードを作ってから実行しないとエラーになります。
+
+user = User.find(1)
+
+Circle.seed do |s|
+  s.id = 1
+  s.name = "サンプルサークル"
+  s.owner = user
+  s.introduction = "サンプルだよ！一緒に遊んでくれると嬉しいです！"
+  s.introduction_picture_url = "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
+  s.default_auto_reply_comment = "参加申請ありがとうございます！ぜひきてください！"
+end
+
+a_week_after = Time.zone.today + 7
+Activity.seed do |s|
+  s.circle = Circle.find(1)
+  s.place_content = PlaceContent.find(1)
+  mex_member_number = 2
+  date = a_week_after
+  start_time = Time.zone.local(a_week_after.year, a_week_after.mon, a_week_after.mday, 16, 0, 0)
+  end_time = Time.zone.local(a_week_after.year, a_week_after.mon, a_week_after.mday, 19, 0, 0)
+end
+
+tomorrow = Time.zone.tomorrow
+Activity.seed do |s|
+  s.circle = Circle.find(1)
+  s.place_content = PlaceContent.find(1)
+  mex_member_number = 2
+  date = a_week_after
+  start_time = Time.zone.local(tomorrow.year, tomorrow.mon, tomorrow.mday, 10, 0, 0)
+  end_time = Time.zone.local(tomorrow.year, tomorrow.mon, tomorrow.mday, 13, 0, 0)
+end
