4272e2212a9eef1ad1d3249bdc94f8b2cc2f159a
nbeat <dev.nbeat@gmail.com>
2018-11-27 14:22:41 +0900

    サークル情報登録処理を追加（紹介画像以外）#11

--- app/app/controllers/webapp/circles_controller.rb
+++ app/app/controllers/webapp/circles_controller.rb
@@ -3,14 +3,25 @@ class Webapp::CirclesController < ApplicationController
   before_action :authenticate
   
   def new
-    @contents = [
-      'フットサル',
-      'バスケ',
-      'バレー',
-      '野球',
-      'バドミントン',
-      'テニス',
-      'その他'
-    ]
+    @contents = EventType.find_by(name: 'スポーツ').contents
+  end
+
+  def create
+    p params
+    circle = Circle.new(circle_params)
+    circle.owner = current_user
+    circle.save
+    params[:content_ids].each do |content_id|
+      circle_content = CircleContent.new
+      circle_content.circle = circle
+      circle_content.content = Content.find(content_id)
+      circle_content.save
+    end
+    redirect_to :action => 'show', :circle_id => circle.id
+  end
+
+  private
+  def circle_params
+    params.require(:circle).permit(:name, :introduction, :default_auto_reply_comment)
   end
 end
