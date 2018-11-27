8c535e67aa6e00c208272034a003b96c1a4def84
nakata yuki <nasutaro211@gmail.com>
2018-11-27 14:36:05 +0900

    fix base_response for circle

--- app/app/lib/line_bot/for_circle/response/image_response.rb
+++ app/app/lib/line_bot/for_circle/response/image_response.rb
@@ -1,19 +1,17 @@
 class LineBot::ForCircle::Response::ImageResponse < LineBot::ForUser::Response::BaseResponse
-  def send(line_bot_event)
+
+  private
+
+  def is_responsible(line_bot_event)
     case line_bot_event
     when Line::Bot::Event::Message
       case line_bot_event.type
       when Line::Bot::Event::MessageType::Image
-        run(line_bot_event)
         return true
       end
     end
-    return false unless @next
-
-    @next.send(line_bot_event)
+    return false
   end
-
-  private
 
   def run(line_bot_event)
     message = {
