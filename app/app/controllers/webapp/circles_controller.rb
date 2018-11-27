8dc09acf06ac9cb6aa5941e0483adce5433896e9
nbeat <dev.nbeat@gmail.com>
2018-11-27 12:25:36 +0900

    [WIP]サークル情報登録画面を追加 #11

--- app/app/controllers/webapp/circles_controller.rb
+++ app/app/controllers/webapp/circles_controller.rb
@@ -3,6 +3,14 @@ class Webapp::CirclesController < ApplicationController
   before_action :authenticate
   
   def new
-    
+    @contents = [
+      'フットサル',
+      'バスケ',
+      'バレー',
+      '野球',
+      'バドミントン',
+      'テニス',
+      'その他'
+    ]
   end
 end
