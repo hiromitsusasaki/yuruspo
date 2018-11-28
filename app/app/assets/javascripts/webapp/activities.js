80ffef7415d0a084e01f041f2c507723244fdef5
nbeat <dev.nbeat@gmail.com>
2018-11-28 23:18:05 +0900

    Google Place APIを使った場所情報のAutocomplete機能を追加

--- app/app/assets/javascripts/webapp/activities.js
+++ app/app/assets/javascripts/webapp/activities.js
@@ -1,2 +1,52 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var autocomplete;
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    (document.getElementById('activity_address')), {
+      type: ['geocode']
+    }
+  )
+  autocomplete.addListener('place_changed', setPlaceInfo);
+}
+
+function setPlaceInfo() {
+  var place = autocomplete.getPlace();
+  var name = place.name
+  var phone_number = place.formatted_phone_number;
+  var address = place.formatted_address;
+  var prefecture_name;
+  var city_name;
+  place.address_components.forEach(function (val, index) {
+    switch (val.types[0]) {
+      case "administrative_area_level_1":
+        prefecture_name = val.long_name;
+        break;
+      case "locality":
+        city_name = val.long_name;
+        break;
+    }
+  });
+  document.getElementById('place_name').setAttribute('value', name);
+  document.getElementById('place_tel').setAttribute('value', phone_number);
+  document.getElementById('place_address').setAttribute('value', address);
+  document.getElementById('prefecture_name').setAttribute('value', prefecture_name);
+  document.getElementById('city_name').setAttribute('value', city_name);
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+    })
+  }
+}
