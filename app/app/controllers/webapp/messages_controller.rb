14dd9972d7c601f580f686dc7cba35e0be548c5b
nbeat <dev.nbeat@gmail.com>
2018-12-01 20:29:27 +0900

    メッセージ送信処理を追加 #17

--- app/app/controllers/webapp/messages_controller.rb
+++ app/app/controllers/webapp/messages_controller.rb
@@ -1,2 +1,21 @@
 class Webapp::MessagesController < ApplicationController
+
+  def create
+    activity = Activity.find(params[:activity_id])
+    message = Message.create(message_params)
+    message.activity = activity
+    message.save
+    activity.users.each do |user|
+      messages = [{type: 'text', text: message.body}]
+      ::LineBot::ForUser::PushWorker.perform_async(user.line_user_id, messages)
+    end
+    redirect_to :controller => 'activities', :action => 'show', :circle_id => activity.circle.id, :activity_id => activity.id
+  end
+
+  private
+  
+  def message_params
+    params.require('message').permit(:body)
+  end
+
 end
