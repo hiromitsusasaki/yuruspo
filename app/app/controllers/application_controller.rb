1f493789670eb49f4b2aa340f0628b8920f901e8
nakata yuki <nasutaro211@gmail.com>
2018-12-03 19:54:37 +0900

    move views/layouts to views/webapp/layouts

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  layout "webapp/layouts/application.html.erb"
+
   protect_from_forgery with: :exception
 
   helper_method :current_user, :logged_in?
 
   private
-  
+
   def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
