8399fcd242ef975b4e72401c4060b8ededa03858
nakata yuki <nasutaro211@gmail.com>
2018-12-05 14:12:42 +0900

    Placeのnameとaddressをpresence: trueにした

--- app/app/models/place.rb
+++ app/app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
   belongs_to :city
   has_many :place_contents
   has_many :contents, through: :place_contents
+
+  validate :name, presence: true
+  validate :address, presence: true
 end
