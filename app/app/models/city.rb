1a5ecb7984e256bde2d11e1f69d5466b1cec61e4
nakata yuki <nasutaro211@gmail.com>
2018-12-05 14:15:22 +0900

    CityとPrefectureのnameをpresencee:trueにした

--- app/app/models/city.rb
+++ app/app/models/city.rb
@@ -2,6 +2,8 @@ class City < ApplicationRecord
   belongs_to :prefecture
   has_many :places
 
+  validate :name, presence: true
+
   def self.where_like(ambiguous_name)
     return self.where(["name LIKE ? OR name LIKE ? OR name LIKE ? OR name LIKE ? OR name LIKE ?", "#{ambiguous_name}%", "%市#{ambiguous_name}", "%市#{ambiguous_name}_", "%郡#{ambiguous_name}", "%郡#{ambiguous_name}_"])
   end
--- app/app/models/prefecture.rb
+++ app/app/models/prefecture.rb
@@ -1,4 +1,6 @@
 class Prefecture < ApplicationRecord
   has_many :cities
   has_many :places, through: :cities
+
+  validate :name, presence: true
 end
