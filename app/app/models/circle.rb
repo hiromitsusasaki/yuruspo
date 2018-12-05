fcd72714dc61c04958805209807702e7fce656c8
nakata yuki <nasutaro211@gmail.com>
2018-12-05 13:47:09 +0900

    サークルの名前をpresence: trueにした

--- app/app/models/circle.rb
+++ app/app/models/circle.rb
@@ -7,6 +7,8 @@ class Circle < ApplicationRecord
   has_many :circle_contents
   has_many :contents, through: :circle_contents
 
+  validate :name, presence: true
+
   def previous_users
     #やりたいことは circle.applicationsのuseridを全て取ってくる。そしてuseridを一つにする。過去の活動なのでactivityのdateは今日よりも前
     return self.users.select("users.*, activities.date").where("date < ?", Time.zone.today).distinct
