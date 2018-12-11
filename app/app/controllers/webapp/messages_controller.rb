3c0112f5c8d879d9cd18a43b6c37717ca831f499
Hiromitsu Sasaki <hiromitsu.sasaki@gmail.com>
2018-12-11 11:33:28 +0900

    メッセージの送信処理にエラーハンドリングを追加 #171

--- app/app/controllers/webapp/messages_controller.rb
+++ app/app/controllers/webapp/messages_controller.rb
@@ -3,14 +3,20 @@ class Webapp::MessagesController < ApplicationController
   before_action :authenticate
 
   def create
-    activity = Activity.find(params[:activity_id])
-    message = Message.create(message_params)
-    message.activity = activity
-    message.save
-    activity.users.each do |user|
-      messages = [{type: 'text', text: message.body}]
-      ::LineBot::ForUser::PushWorker.perform_async(user.line_user_id, messages)
+    ActiveRecord::Base.transaction do
+      activity = Activity.find(params[:activity_id])
+      message = Message.create(message_params)
+      message.activity = activity
+      message.save
+      activity.users.each do |user|
+        messages = [{type: 'text', text: message.body}]
+        ::LineBot::ForUser::PushWorker.perform_async(user.line_user_id, messages)
+      end
+      flash[:success] = "メッセージを送信しました"
+      redirect_to :controller => 'activities', :action => 'show', :circle_id => activity.circle.id, :activity_id => activity.id
     end
+  rescue
+    flash[:success] = "メッセージの送信に失敗しました"
     redirect_to :controller => 'activities', :action => 'show', :circle_id => activity.circle.id, :activity_id => activity.id
   end
 
